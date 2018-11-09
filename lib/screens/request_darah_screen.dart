4fc38bbc3178396a023f3dfa22abbaa6e3713e7f
Dwiki Nugraha <skeithnight@gmail.com>
2018-11-09 17:29:35 +0700

    map view, tapi ga dapet api key

--- lib/screens/request_darah_screen.dart
+++ lib/screens/request_darah_screen.dart
@@ -1,50 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:map_view/map_view.dart';
 
 import 'package:blood/data.dart' as data;
 
-class RequestDarahScreen extends StatelessWidget {
-  Future<Position> posisiSekarang() async {
-    await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+class RequestDarahScreen extends StatefulWidget {
+  _RequestDarahScreenState createState() => _RequestDarahScreenState();
+}
+
+class _RequestDarahScreenState extends State<RequestDarahScreen> {
+  MapView mapView = new MapView();
+
+  displayMap() {
+    MapOptions mapOptions = new MapOptions(
+        mapViewType: MapViewType.normal,
+        initialCameraPosition: CameraPosition(Location(35.22, -101.83), 15.0), showMyLocationButton: true, showUserLocation: true, title: 'Lokasi permintaan');
+    mapView.show(mapOptions);
   }
 
-  Future<GeolocationStatus> statusGeoLocation() async {
-    await Geolocator().checkGeolocationPermissionStatus();
-  }
-
-  void getPosition() {
-    // print("lokasi: "+posisiSekarang().toString());
-    statusGeoLocation().then((onValue) {
-      print(onValue);
-    });
-  }
-
-  Widget content(context) => new Scaffold(
-        appBar: AppBar(),
-        bottomNavigationBar: Container(
-          margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: new RaisedButton(
-            child: const Text('Request'),
-            textColor: Colors.white,
-            color: Colors.green,
-            elevation: 4.0,
-            splashColor: Colors.blueGrey,
-            onPressed: () {
-              getPosition();
-              // Perform some action
-            },
+  Widget content() => new Scaffold(
+        appBar: AppBar(
+          title: Text("Request Darah"),
+        ),
+        body: Center(
+          child: Container(
+            child: RaisedButton(
+              child: Text("Tap me"),
+              color: Colors.blue,
+              textColor: Colors.white,
+              elevation: 7.0,
+              onPressed: displayMap(),
+            ),
           ),
         ),
-        body: new Center(
-            // child: getPosition(),
-            ),
       );
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return SafeArea(
-      child: content(context),
+    return new MaterialApp(
+      home: content(),
     );
   }
 }
